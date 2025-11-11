1067cd289cdaa8ad440f38341413973bba1c9fa7
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2025-11-11 13:48:36 +0100

    Update Ejercicio17.py

--- UD2/Ejercicio17.py
+++ UD2/Ejercicio17.py
@@ -1,6 +1,8 @@
 tareas_dia = ["Revisar Email", "Llamar al cliente", "comprar café", "Actualizar reporte", "Planificar reunión"]
 
 tareas_orden = sorted(tareas_dia, key=str.lower)
+# Quitar la almohadilla para descomentarlo
+# tareas_dia.sort(key=len) 
 
 print(tareas_orden)
 
